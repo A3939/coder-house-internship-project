9bf2ef831146f3897a8047b2b5f84ea30bc6d76d
theonetech191 <Aditya.Patel>
2023-03-03 19:25:00 +0530

    405 error

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'http://localhost:5500',
     // baseURL: process.env.REACT_APP_API_URL,
+    withCredentials: true,
     headers: {
         'Content-type': 'application/json',
         Accept: 'application/json',
@@ -13,4 +14,5 @@ const api = axios.create({
 // List of all the endpoints
 export const sendOtp = (data) => api.post('/api/send-otp', data);
 export const verifyOtp = (data) => api.post('/api/verify-otp', data);
+export const activate = (data) => api.post('/api/activate', data);
 export default api;
